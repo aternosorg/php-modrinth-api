c0161794a0f69d0aafaa0ee30578eb84d836cc98
Julian <julian@aternos.org>
2023-05-25 12:53:49 +0200

    add getProject to Version

--- lib/Client/Version.php
+++ lib/Client/Version.php
@@ -2,6 +2,7 @@
 
 namespace Aternos\ModrinthApi\Client;
 
+use Aternos\ModrinthApi\ApiException;
 use Aternos\ModrinthApi\Model\Version as VersionModel;
 
 class Version
@@ -19,5 +20,15 @@ class Version
     public function getData(): VersionModel
     {
         return $this->data;
+    }
+
+    /**
+     * Fetch the full project from the API
+     * @return Project
+     * @throws ApiException
+     */
+    public function getProject(): Project
+    {
+        return $this->client->getProject($this->data->getProjectId());
     }
 }
