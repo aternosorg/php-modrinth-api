65664c0ce4e70a1a5f25d6341cc887fc8f9ec4f6
Julian <julian@aternos.org>
2024-01-09 11:30:41 +0100

    Add method to get all results from all pages to PaginatedList

--- lib/Client/List/PaginatedList.php
+++ lib/Client/List/PaginatedList.php
@@ -114,7 +114,25 @@ abstract class PaginatedList implements Iterator, ArrayAccess, Countable
         return $this->getOffset($this->getPreviousOffset());
     }
 
-
+    /**
+     * Get all results from this page and all following pages.
+     * This will request each page from the api one by one.
+     *
+     * When called on the first page this will return all results.
+     *
+     * @throws ApiException
+     * @return T[]
+     */
+    public function getResultsFromFollowingPages(): array
+    {
+        $results = $this->getResults();
+        $nextPage = $this->getNextPage();
+        while ($nextPage !== null) {
+            array_push($results, ...$nextPage->getResults());
+            $nextPage = $nextPage->getNextPage();
+        }
+        return $results;
+    }
 
     /**
      * @inheritDoc
